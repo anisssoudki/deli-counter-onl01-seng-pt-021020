d5b38fb192246a3a537a2b739f23b1f3cfe5e684
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-13 20:36:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def line(array) 
+  if array == []
+    puts "the line is currently empty"
+    
+  else
+phrase = "The line is currently: "
+  	array.each do 
+
+    puts phrase.concat"#{ array.index(i)+1}. #{i} "
+    end
+
+  end
+end
+
+
+def line(array) 
+  if array == []
+    puts "The line is currently empty."
+    
+  else
+  	counter = 1
+		array.each do |person|
+
+			puts "The line is currently: #{person} #{counter}"
+counter +=1
+    end
+
+  end
+end
+
