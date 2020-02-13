270ba2a7fd9371f23459e9dd74bd8721c4e06142
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-13 22:58:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(array, person)
  array.push(person)
- puts "Welcome, #{person}. You are number #{array.length} in line"
+ puts "Welcome, #{person}. You are number #{array.length} in line."
  
 end
 
